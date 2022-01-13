ac19d4feb000a9ede26497994205096608ff94d6
harsh-javascript <harsh.patel@simformsolutions.com>
2022-01-13 17:47:49 +0530

    functions and conditions

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var i;
+console.log(i);
 i = 1;
 document.getElementById("name").innerHTML = "first var" + i;
 
-var newLet;
+let newLet;
 newLet = 0; //initialization can be done later for var
 document.getElementById("let").innerHTML += "<br>" + newLet;
 
@@ -12,3 +13,15 @@ function passValue() {
 }
 passValue();
 document.getElementById("let").innerHTML += "<br>" + newLet;
+
+function hello() {
+  let val = prompt("let me check whether your fav. num is even or odd?");
+  console.log(typeof val);
+  if (val == "") {
+    alert("you don't want to say your fav num!!!");
+  } else if (val % 2 != 0) {
+    alert("your fav. num is odd.");
+  } else {
+    alert("your fav. num is even.");
+  }
+}
