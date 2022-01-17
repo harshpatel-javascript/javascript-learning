1d40a843b9e613efa3410b76fcc54dd6b2d7e2f4
harsh-javascript <harsh.patel@simformsolutions.com>
2022-01-17 12:39:25 +0530

    conditions and loops

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var i;
-console.log(i);
+console.log("checking the variable without initialization.   " + i); //gives undefined as value of i isn't defined
 i = 1;
 document.getElementById("name").innerHTML = "first var" + i;
 
 let newLet;
 newLet = 0; //initialization can be done later for var
 document.getElementById("let").innerHTML += "<br>" + newLet;
+
+// const j;
+// j=0; //gives error : Uncaught SyntaxError: Missing initializer in const declaration
+// console.log(j);
 
 function passValue() {
   let newLet = "hello there";
@@ -14,6 +18,7 @@ function passValue() {
 passValue();
 document.getElementById("let").innerHTML += "<br>" + newLet;
 
+//conditions
 function hello() {
   let val = prompt("let me check whether your fav. num is even or odd?");
   console.log(typeof val);
