21d1e455cf463d09994edfcca1b3d45b767bd674
harsh-javascript <harsh.patel@simformsolutions.com>
2022-01-18 14:05:21 +0530

    added splice method example

--- array.js
+++ array.js
@@ -21,6 +21,21 @@ console.log(arr);
 arr.unshift(2);
 console.log(arr);
 
-//sort method : because it compares the string of array so 10
+//sort method : because it compares the string of array
 arr.sort();
 console.log(arr);
+
+//splice method : it inserts deltes and override 
+let arr1 = ["I", "study", "JavaScript"];
+
+//from index 0
+//delete 2 element
+let removed = arr1.splice(0,2);
+alert(removed); // delted element would be "I" , "study"
+
+// from index 2
+// delete 0
+// then insert "complex" and "language"
+arr1.splice(2, 0, "complex", "language");
+
+alert( arr1 ); // "I", "study", "complex", "language", "JavaScript"
