b411114e05d1955b261104fb004ff48b17aa8b50
harsh-javascript <harsh.patel@simformsolutions.com>
2022-01-18 14:44:03 +0530

    learned about objects

---
+++ objects.js
@@ -0,0 +1,27 @@
+// let user  =  new Object() //object constructor
+let user = {
+    name :"mika",
+    legs:4,
+    tails:1,
+    friends:["tucker","bryan","chat"]
+}; //object literal
+
+// if we want to access the object properties : 1st way
+const userName = user.name;
+const userFriends = user.friends;
+console.log(userName);
+//:2nd way
+const userLegs = user['legs'];
+console.log("user has " + userLegs + " legs");
+
+//updating the value
+user.name = "mika the husky";
+console.log(user.name);
+
+//adding the new properties same way we are accessing them
+user.bark = "woof";
+console.log(user);
+
+//deleting the properties
+delete user.tails;
+console.log(user);
